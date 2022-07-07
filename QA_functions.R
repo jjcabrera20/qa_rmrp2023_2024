5f0c02828ac44d83868babf3abbc72bfbf641753
jjcabrera20 <jjcabrera20@yahoo.com.mx>
2022-07-07 09:27:11 -0500

    check for integers, labeled

--- QA_functions.R
+++ QA_functions.R
@@ -1,11 +1,14 @@
 library(dplyr)
-#library(tidyr)
-#library(tidyverse)
+
 
 checkNumberOfRecords <- function(template, countrydata, joinFields){
   anti_join_a = anti_join(template, countrydata, joinFields)
+  anti_join_a['Status'] <- "Admin1 missing from template"
   anti_join_b = anti_join(countrydata, template, joinFields)
+  anti_join_b['Status'] <- "Category not found in template, check spelling"
+  reportFields = append(joinFields, 'Status')
   no_match = rbind(anti_join_a,anti_join_b)
+  no_match = no_match[,reportFields]
   return (no_match)
 }
 
@@ -13,8 +16,5 @@ naToZero <- function (x) {
   return(x %>% replace(is.na(.), 0))
 }
 
-isWholeNumber <- function(x) {floor(x)==x}
+isWholeNumber <- function(x) {ifelse(floor(x)==x,"","Review")}
 
-# isWholeNumber <- function(x) {
-#   return(all(floor(x)==x))
-# }
--- mainQAScript.R
+++ mainQAScript.R
@@ -13,14 +13,18 @@ POPULATION_PROJECTION_COUNTRY_FILE = "Ecuador_pop_projection.xlsx"
 POPULATION_PROJECTION_FILE_PATH = paste(BASE_PATH, POPULATION_PROJECTION_TEMPLATE_FILE, sep = "/")
 df_population_template = read_excel(POPULATION_PROJECTION_FILE_PATH)
 df_population_template = filter(df_population_template, df_population_template$Country==COUNTRY)
+df_population_template$index <- 1:nrow(data)
 # Import excel files population projections files
 POPULATION_PROJECTION_COUNTRY_PATH = paste(BASE_PATH, POPULATION_PROJECTION_COUNTRY_FILE, sep = "/")
 df_population_country = read_excel(POPULATION_PROJECTION_COUNTRY_PATH)
+df_population_country = filter(df_population_country, df_population_country$Country==COUNTRY)
+
+# COMPLETENESS ---- 
 
 df_completeness = checkNumberOfRecords(df_population_template,df_population_country, c("Platform", "Country", "Admin 1"))
 if(nrow(df_completeness) > 0){
   print("Data not compliant with template, check Platform, Country and Admin1 names")
-
+  # TODO FALTA o SOBRA en una sola columna
   write_xlsx(df_completeness,paste(BASE_PATH,paste("Admin1NoCompliant_",POPULATION_PROJECTION_COUNTRY_FILE),sep="/"))
 }
 df_population_country <- naToZero(df_population_country)
@@ -30,4 +34,4 @@ numCols_pop_country<-names(df_population_country)[sapply(df_population_country, 
 
 numData_pop_country<- df_population_country %>% select(all_of(numCols_pop_country))
 
-IsRound_numData_pop_country <- data.frame(lapply( numData_pop_country, function(x) {floor(x)==x}))
+IsRound_numData_pop_country <- data.frame(lapply( numData_pop_country, isWholeNumber))
