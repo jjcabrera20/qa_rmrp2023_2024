514661ca7d05ef74e8c40fe4afad50ef385276d3
jjcabrera20 <jjcabrera20@yahoo.com.mx>
2022-07-08 17:04:22 -0500

    documentation included

--- QA_functions.R
+++ QA_functions.R
@@ -2,20 +2,66 @@ library(dplyr)
 
 # QA Functions ----
 
-checkNumberOfRecords <- function(template, countrydata, joinFields){
-  anti_join_a = anti_join(template, countrydata, joinFields)
+#' Check field compliance 
+#'
+#' @param template data frame 
+#' @param ncountryData data frame
+#'
+#' @return string with field differences
+#' @export
+#'
+#' @examples
+#' checkFieldsCompliantWithTemplate(df_population_template, df_population_country)
+checkFieldsCompliantWithTemplate <- function(template, countryData){
+  difference_a = setdiff(colnames(template), colnames(countryData))
+  difference_b = setdiff(colnames(countryData), colnames(template))
+  paste(difference_a,difference_b, sep=" ")
+  return(paste("Non compliant columns", paste(difference_a,difference_b, sep=" "),sep=" "))
+}
+#' Check records compliance 
+#'
+#' @param template data frame
+#' @param countryData data frame
+#' @param joinfields vector
+#'
+#' @return data frame with non compliant records, if all records are compliant
+#' returns empty data frame
+#' @export
+#'
+#' @examples
+#' checkRecordsCompliancy(df_population_template,df_population_country,
+#' c("Platform", "Country", "Admin 1"))
+checkRecordsCompliancy <- function(template, countryData, joinFields){
+  anti_join_a = anti_join(template, countryData, joinFields)
   anti_join_a['Status'] <- "Admin1 missing from template"
-  anti_join_b = anti_join(countrydata, template, joinFields)
+  anti_join_b = anti_join(countryData, template, joinFields)
   anti_join_b['Status'] <- "Category not found in template, check spelling"
   reportFields = append(joinFields, 'Status')
   no_match = rbind(anti_join_a,anti_join_b)
   no_match = no_match[,reportFields]
   return (no_match)
 }
-
+#'Convert NA to Zero
+#'
+#' @param dataframe
+#' Condition, all columns must be of number type
+#' @return dataframe with NA values converted to Zero
+#' @export
+#'
+#' @examples
+#' naToZero(df_population_country)
 naToZero <- function (x) {
   return(x %>% replace(is.na(.), 0))
 }
 
+#'Check if stored number is round
+#'
+#' @param dataframe
+#' Condition, all columns must be of number type
+#' @return dataframe with flagged non round numbers
+#' @export
+#'
+#' @examples
+#' IsRound_numData_pop_country <- data.frame(lapply( numData_pop_country, isWholeNumber))
 isWholeNumber <- function(x) {ifelse(floor(x)==x,"","Review")}
 
--- mainQAScript.R
+++ mainQAScript.R
@@ -13,21 +13,23 @@ POPULATION_PROJECTION_COUNTRY_FILE = "Ecuador_pop_projection.xlsx"
 POPULATION_PROJECTION_FILE_PATH = paste(BASE_PATH, POPULATION_PROJECTION_TEMPLATE_FILE, sep = "/")
 df_population_template = read_excel(POPULATION_PROJECTION_FILE_PATH)
 df_population_template = filter(df_population_template, df_population_template$Country==COUNTRY)
-df_population_template$index <- 1:nrow(data)
 # Import excel files population projections files
 POPULATION_PROJECTION_COUNTRY_PATH = paste(BASE_PATH, POPULATION_PROJECTION_COUNTRY_FILE, sep = "/")
 df_population_country = read_excel(POPULATION_PROJECTION_COUNTRY_PATH)
-df_population_country = filter(df_population_country, df_population_country$Country==COUNTRY)
+
+# INTEGRITY ----
+a<-checkFieldsCompliantWithTemplate(df_population_template, df_population_country)
 
 # COMPLETENESS ---- 
-
-df_completeness = checkNumberOfRecords(df_population_template,df_population_country, c("Platform", "Country", "Admin 1"))
+df_population_country = filter(df_population_country, df_population_country$Country==COUNTRY)
+df_completeness = checkRecordsCompliancy(df_population_template,df_population_country, c("Platform", "Country", "Admin 1"))
 if(nrow(df_completeness) > 0){
   print("Data not compliant with template, check Platform, Country and Admin1 names")
   write_xlsx(df_completeness,paste(BASE_PATH,paste("Admin1NoCompliant_",POPULATION_PROJECTION_COUNTRY_FILE),sep="/"))
 }
 df_population_country <- naToZero(df_population_country)
 
+# CONSISTENCY ----
 #Get columns with numeric data
 numCols_pop_country<-names(df_population_country)[sapply(df_population_country, is.numeric)]
 
